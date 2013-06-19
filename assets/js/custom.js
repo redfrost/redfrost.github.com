c4698fe5cc93edf5d33b57405a8d130342374393
Redfrost <contact@simonalex.com>
2013-06-19 22:01:44 +1000

    Scroll fading banner

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -15,6 +15,29 @@
     });
 
 
+// Parallax 
+    jQuery(document).ready(function () {
+        var fadeStart=0 // 100px scroll or less will equiv to 1 opacity
+            ,fadeUntil=360 // 200px scroll or more will equiv to 0 opacity
+            ,fading = $('.scroll-fade');
+        
+        $(window).bind('scroll', function(){
+            var offset = $(document).scrollTop()
+                ,opacity=0;
+            
+            if( offset<=fadeStart ){
+                opacity=1;
+            
+            }else if ( offset<=fadeUntil ){
+                opacity=1-offset/fadeUntil;
+            }
+            
+            fading.css('opacity',opacity);
+        });
+    });
+
+    
+
 // Tooltip
     jQuery(document).ready(function () {
         $("[rel=tooltip]").tooltip();
