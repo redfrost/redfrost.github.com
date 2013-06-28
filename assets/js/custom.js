92b078de569b6cf3d22baef18fbd9cb2be17cf95
Redfrost <contact@simonalex.com>
2013-06-29 04:14:30 +1000

    testing

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -81,7 +81,32 @@
 		
 // Add your own		
 	
-	
+	   setTimeout(function(){
+
+        $('.progress .bar').each(function() {
+            var me = $(this);
+            var perc = me.attr("data-percentage");
+
+            //TODO: left and right text handling
+
+            var current_perc = 0;
+
+            var progress = setInterval(function() {
+                if (current_perc>=perc) {
+                    clearInterval(progress);
+                } else {
+                    current_perc +=1;
+                    me.css('width', (current_perc)+'%');
+                }
+
+                me.text((current_perc)+'%');
+
+            }, 50);
+
+        });
+
+    },300);
+
 		
 // Preloader - Hide content until the page is fully loaded
     $(document).ready(function() {
@@ -100,3 +125,4 @@
     //    });
     //});
 		
+ 
